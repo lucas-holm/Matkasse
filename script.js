5d6323095a645b450ffd8dd14a3bc47aa3506908
AlexEllner <AlexEllner@users.noreply.github.com>
2020-11-03 09:17:04 +0100

    Initial commit

--- script.js
+++ script.js
@@ -10,21 +10,21 @@ const kasse3 = document.querySelector("#kasse-3");
 const close = document.querySelectorAll(".close");
 
 card1.addEventListener("click", () => {
-    kasse1.style.display = "block";
+  kasse1.style.display = "block";
 });
 
 card2.addEventListener("click", () => {
-    kasse2.style.display = "block";
+  kasse2.style.display = "block";
 });
 
 card3.addEventListener("click", () => {
-    kasse3.style.display = "block";
+  kasse3.style.display = "block";
 });
 
 close.forEach((item) => {
-    item.addEventListener("click", () => {
-        kasse1.style.display = "none";
-        kasse2.style.display = "none";
-        kasse3.style.display = "none";
-    });
+  item.addEventListener("click", () => {
+    kasse1.style.display = "none";
+    kasse2.style.display = "none";
+    kasse3.style.display = "none";
+  });
 });
